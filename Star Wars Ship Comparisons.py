40cc2c51ac1fc1b84c7e044d517e4d6d0550105d
Michael-Manz <pastormanz@gmail.com>
2022-10-06 15:33:02 -0400

    First Edit

--- Star Wars Ship Comparisons.py
+++ Star Wars Ship Comparisons.py
@@ -1,5 +1,12 @@
 # Install all needed modules for this program.
-import matplotlib as plt
+import csv
 import pandas as pd
+import matplotlib as plt
+
+# Read in data using pandas.
+starships = pd.read_csv('Star Wars Starships.csv')
+print(starships.head(5))
+
+# Clean data using pandas.
 
 
